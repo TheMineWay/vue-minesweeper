92e495b4d54c42ea6ee05436d976d7dfe1bf4dee
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-10 20:38:22 +0200

    Basic hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Main page</h1>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Minesweeper</h1>
+        <p class="py-6">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <button class="btn btn-primary">Start game</button>
+      </div>
+    </div>
+  </div>
 </template>
