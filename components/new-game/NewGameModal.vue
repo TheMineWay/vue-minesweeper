eeab7c92e279a10ff31d09e81771d022c51a1af8
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-11 15:30:14 +0200

    New game form

--- components/new-game/NewGameModal.vue
+++ components/new-game/NewGameModal.vue
@@ -1,10 +1,32 @@
 <template>
   <dialog id="new_game_modal" class="modal modal-bottom sm:modal-middle">
-    <form method="dialog" class="modal-box">
+    <form method="dialog" class="modal-box flex flex-col gap-4">
       <h3 class="font-bold text-lg">
         {{ $t("main-page.new-game-modal.Title") }}
       </h3>
-      <p class="py-4"></p>
+      <div>
+        <form>
+          <div class="flex flex-col gap-4">
+            <div class="flex gap-4">
+              <input
+                type="number"
+                :placeholder="$t('main-page.new-game-modal.form.Height')"
+                class="input input-bordered w-full max-w-xs"
+              />
+              <input
+                type="number"
+                :placeholder="$t('main-page.new-game-modal.form.Width')"
+                class="input input-bordered w-full max-w-xs"
+              />
+            </div>
+            <div class="w-full">
+              <button class="btn btn-primary w-full">
+                {{ $t("main-page.new-game-modal.form.Start") }}
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </form>
     <form method="dialog" class="modal-backdrop">
       <button>close</button>
