d0322d60f859e4113289dcfd29276d77fea5c1e1
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-11 09:59:47 +0200

    Align

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Minesweeper</h1>
-        <p class="py-6">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
-        <button class="btn btn-primary">Start game</button>
+  <div class="h-full">
+    <div class="hero bg-base-200 h-full">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Minesweeper</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary">Start game</button>
+        </div>
       </div>
     </div>
   </div>
