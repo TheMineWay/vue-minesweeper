0d9723a23f0ff55967ba612ae21f5146dafe4383
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-11 15:04:15 +0200

    no PWA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@vite-pwa/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
   i18n: {
     locales: [
       {
@@ -15,13 +15,5 @@ export default defineNuxtConfig({
     langDir: "locales/",
   },
   components: [{ path: "~/components", pathPrefix: false }],
-  pwa: {
-    manifest: {
-      name: "Minesweeper",
-      description: "A web based Minesweeper game",
-      lang: "en",
-      useWebmanifestExtension: false,
-    },
-  },
   ssr: false,
 });
