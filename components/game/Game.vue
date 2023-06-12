0b28d355341b9a8a47d62dd3e703d40ced09da75
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-12 19:17:08 +0200

    Unrevealed

--- components/game/Game.vue
+++ components/game/Game.vue
@@ -10,11 +10,13 @@ const bombed = ref<boolean>(false);
 const boardState = ref<BoardCell[][]>(props.game.getBoard().getBoard());
 
 const onRevealCell = (x: number, y: number) => {
+  if (bombed.value) return;
   const board = boardState.value;
   boardState.value = [...revealCell(board, x, y)];
 };
 
 const onFlagCell = (x: number, y: number) => {
+  if (bombed.value) return;
   const board = boardState.value;
   const cell = board[x][y];
   if (cell.isRevealed()) return;
@@ -82,6 +84,7 @@ const revealCell = (board: BoardCell[][], x: number, y: number) => {
                 d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12.435 12.435 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A19.626 19.626 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a19.587 19.587 0 0 0 1.349-.476l.019-.007.004-.002h.001"
               />
             </svg>
+            <div v-else class="unrevealed"></div>
           </div>
         </td>
       </tr>
@@ -101,6 +104,11 @@ table.map {
         display: flex;
         justify-content: center;
         align-items: center;
+      }
+      div.unrevealed {
+        background-color: white;
+        width: 2em;
+        height: 2em;
       }
     }
   }
