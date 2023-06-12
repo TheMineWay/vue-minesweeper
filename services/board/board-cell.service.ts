d98088bce1a7f66bf608ec1978f126b0168dcc1c
Joel Campos <joelcamposoliva9000@gmail.com>
2023-06-12 18:08:06 +0200

    Detect right click

--- services/board/board-cell.service.ts
+++ services/board/board-cell.service.ts
@@ -1,8 +1,13 @@
 export class BoardCell {
+  private revealed: boolean = false;
+
   private constructor(
     public readonly hasMine: boolean,
     public readonly isFlagged: boolean = false
   ) {}
+
+  public isRevealed = () => this.revealed;
+  public reveal = () => (this.revealed = true);
 
   public static CreateMine = () => new BoardCell(true);
   public static CreateEmpty = () => new BoardCell(false);
